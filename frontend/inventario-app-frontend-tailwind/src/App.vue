4fe91855f0f0adb65acec89d8d2eacb5e7e66af4
Jishiro <kronosiraxx@gmail.com>
2025-07-17 16:43:25 -0500

    Segundo intento

---
+++ frontend/inventario-app-frontend-tailwind/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-gray-100">
+    <h1 class="text-4xl font-bold text-indigo-600">¡Tailwind está funcionando! 🎉</h1>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
